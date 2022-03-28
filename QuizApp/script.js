d5462523d77f3d096a5789a4e9c46c034272091c
unknown <russelhkamal@gmail.com>
2022-03-27 21:56:49 -0400

    created questions array

---
+++ QuizApp/script.js
@@ -0,0 +1,27 @@
+var Questions = [
+    {   
+        question: "What does html stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Makeup Language", "Hyperr Make Type Language", "Cascading Style Sheets"],
+        answer: "Hypertext Markup Language"
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables?",
+        options: ["Curly brackets","Exclamation marks", "Parenthesis", "Quotation Marks"],
+        answer : "Quotation Marks"
+    },
+    {
+        question: "Which is not a commonly used data type in coding?",
+        options: ["boolean","integer", "pounds", "string"],
+        answer : "pounds"
+    },
+    {
+        question: "Which of the following is a commonly used coding language?",
+        options: ["Latte","Mocha", "Espresso", "Java"],
+        answer : "Java"
+    },
+    {
+        question: "How should you NOT react when encountering an error in your code?",
+        options: ["Take a small break to come back with a fresh mind","Look for the error message and see what is wrong", "Throw your computer across the room","Read over your code to look for typos or logic errors"],
+        answer : "Throw your computer across the room"
+    }
+]
